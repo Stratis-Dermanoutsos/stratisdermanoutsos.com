f904e5feeb391bbfc676f80673575479310f13fa
Stratis Dermanoutsos <stratis450@gmail.com>
2023-07-15 00:38:55 +0300

    Update layout.ts

--- src/stores/layout.ts
+++ src/stores/layout.ts
@@ -2,7 +2,7 @@ import { atom } from 'nanostores';
 
 export const $isSidemenuOpen = atom<boolean>(false);
 
-export const toggleSidemenu = (newValue: boolean | undefined) => {
+export const toggleSidemenu = (newValue?: boolean | undefined) => {
     if (newValue !== undefined)
         $isSidemenuOpen.set(newValue);
     else
