be241c903e9eec1dd56275c1321e59ebefbcca8a
Stratis Dermanoutsos <stratis450@gmail.com>
2023-07-15 00:39:15 +0300

    Added sidemenu items to Consts

--- src/Consts.ts
+++ src/Consts.ts
@@ -1,2 +1,28 @@
 export const TITLE_PREFIX = 'Stratis Dermanoutsos';
 export const DESCRIPTION = 'Stratis Dermanoutsos is a Senior Software Developer based in Athens, Greece. He is passionate about building software in the most efficient and maintainable way possible.';
+
+export const SIDEMENU_ITEMS = [{
+    title: 'Home',
+    url: '/',
+    icon: 'Home'
+}, {
+    title: 'About',
+    url: '/about',
+    icon: 'AdressCard'
+}, {
+    title: 'Work',
+    url: '/work',
+    icon: 'Briefcase'
+}, {
+    title: 'Blog',
+    url: '/blog',
+    icon: 'CommentDots'
+}, {
+    title: 'Notes',
+    url: 'https://notes.stratisdermanoutsos.com',
+    icon: 'Book'
+}, {
+    title: 'About this page',
+    url: '/about-this-page',
+    icon: 'CircleInfo'
+}]
