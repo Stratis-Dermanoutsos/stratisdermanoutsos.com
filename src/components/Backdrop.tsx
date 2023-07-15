dd334b88a4af7b653d1e4c3ba28dee956e4175d0
Stratis Dermanoutsos <stratis450@gmail.com>
2023-07-15 14:24:26 +0300

    Added backdrop
    It blocks content when the sidebar is shown in mobile.

---
+++ src/components/Backdrop.tsx
@@ -0,0 +1,16 @@
+import { useStore } from '@nanostores/react';
+import { $isSidemenuOpen, toggleSidemenu } from '../stores/layout';
+
+const Backdrop = () => {
+    const isSidemenuOpen = useStore($isSidemenuOpen);
+    const clickEvent = () => toggleSidemenu(false);
+
+    const commonClasses = 'bg-black fixed inset-0 z-[998]';
+    const className = isSidemenuOpen
+        ? `${commonClasses} bg-opacity-50`
+        : `${commonClasses} bg-opacity-0 pointer-events-none`;
+
+    return <div className={className} onClick={clickEvent} />;
+};
+
+export default Backdrop;
